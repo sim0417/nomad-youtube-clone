e22333f69a510841a405c4af3732d8d1e998ba56
EonJo <sim04170@gmail.com>
2022-02-01 23:48:21 +0900

    [ Modify ] add logger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const SERVER_PORT = process.env.SERVER_PORT;
 const app = express();
+const logger = morgan('dev');
 
 const handleServerStart = () => {
   console.log(`Server listening on http://localhost:${SERVER_PORT}`);
 };
 
+app.use(logger);
 app.listen(SERVER_PORT, handleServerStart);
