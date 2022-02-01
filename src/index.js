793c75a44dd65c116ec793c13f3a312d03672321
EonJo <sim04170@gmail.com>
2022-02-01 22:10:57 +0900

    [ Add ] build server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const SERVER_PORT = process.env.SERVER_PORT;
+const app = express();
+
+const handleServerStart = () => {
+  console.log(`Server listening on http://localhost:${SERVER_PORT}`);
+};
+
+app.listen(SERVER_PORT, handleServerStart);
