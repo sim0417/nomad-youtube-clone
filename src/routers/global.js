4b4c3990da7bf0519e909cd367b23d8a79c065f3
EonJo <sim04170@gmail.com>
2022-03-02 00:29:36 +0900

    [ Add ] base layout file

--- src/routers/global.js
+++ src/routers/global.js
@@ -2,9 +2,7 @@ import express from 'express';
 
 export default function globalRouter() {
   const router = express.Router();
-  const getHomepage = (req, res) => {
-    res.render('home');
-  };
+  const getHomepage = (req, res) => res.render('home', { pageTitle: 'Home' });
   router.get('/', getHomepage);
 
   return router;
