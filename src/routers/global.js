62d905bda3fdc24550a18dc22a70434c12a27fb4
EonJo <sim04170@gmail.com>
2022-03-06 22:19:12 +0900

    [ Add ] test Mixin & iteration

--- src/routers/global.js
+++ src/routers/global.js
@@ -2,7 +2,21 @@ import express from 'express';
 
 export default function globalRouter() {
   const router = express.Router();
-  const getHomepage = (req, res) => res.render('home', { pageTitle: 'Home' });
+  const getHomepage = (req, res) =>
+    res.render('home', {
+      pageTitle: 'Home',
+      videos: [
+        {
+          label: 'test1',
+        },
+        {
+          label: 'test2',
+        },
+        {
+          label: 'test3',
+        },
+      ],
+    });
   router.get('/', getHomepage);
 
   return router;
