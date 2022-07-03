baf24a18691eb338b11ce4e26fb394d87b2a5372
EonJo <sim04170@gmail.com>
2022-07-03 23:33:00 +0900

    [ modify ] edit page template

--- src/controllers/videos.js
+++ src/controllers/videos.js
@@ -17,7 +17,18 @@ export const watchVideo = (req, res) => {
 };
 
 export const editVideo = (req, res) => {
+  const { id } = req.params;
+  const video = {
+    title: 'Test',
+    rating: 5,
+    comments: 2,
+    createdAt: '2022-03-06',
+    views: 100,
+    id,
+  };
+
   res.render('edit', {
-    pageTitle: 'Edit Video',
+    pageTitle: `Editting : ${video.title}`,
+    video,
   });
 };
